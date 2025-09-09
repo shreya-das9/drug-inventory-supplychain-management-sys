eb01ac13edf1c751f9874d42fc4a75fab27232e5
Susneha Ghosh <146771281+Othello-Von-Ryan@users.noreply.github.com>
2025-09-09 14:53:59 +0530

    Update auth.api.js
    Updated for forgot password

--- client/src/services/auth.api.js
+++ client/src/services/auth.api.js
@@ -1,3 +1,7 @@
 import api from "./api";
+
 export const signup = (data) => api.post("/auth/signup", data);
-export const login  = (data) => api.post("/auth/login", data);
+export const login = (data) => api.post("/auth/login", data);
+export const forgotPassword = (data) => api.post("/auth/forgot-password", data);
+export const resetPassword = (data) => api.post("/auth/reset-password", data);
+export const verifyResetToken = (token) => api.get(`/auth/verify-reset-token/${token}`);
